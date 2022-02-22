f6b3f076bf0821fc0d8535bbaa9612c3c5c35150
eloctro <eloctro@tut.by>
2022-02-22 15:34:28 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = "";
+    let arr = [];
+
+    if (n < 0) {
+        n *= -1;
+    }
+    str = n.toString();
+    for (let i = str.length - 1; i >= 0; i--) {
+        arr.push(str[i]);
+    }
+    str = arr.join("");
+    return +str;
+};
